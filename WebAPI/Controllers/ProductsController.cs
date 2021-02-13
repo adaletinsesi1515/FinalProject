76eb242ca23b7d54beb31797f214288b61787cb3
Memduh Gümen <75331205+adaletinsesi1515@users.noreply.github.com>
2021-02-13 22:47:31 +0300

    13 Şubat 1.commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  //Attribute  - bir class ile ilgili bilgi verme ve imzalama biçimidir. 
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled --> gevşek bağlılık - soyuta bağımlılık var 
+        //IoC container --> Değişimin kontrolü
+
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency Chain --> Bağımlılık zinciri
+
+            var result = _productService.GetAll();
+            return result.Data;
+
+        }
+    }
+}
